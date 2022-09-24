56b2f0e79ab8dbf59291939d37b46b5a6180d159
BadCodedByAFrench <100372895+BadCodedByAFrench@users.noreply.github.com>
2022-09-24 13:42:31 +0200

    Update requests.js

--- util/requests.js
+++ util/requests.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-import { useQuery  } from "react-query";
-import { GraphQLClient, gql } from "graphql-request";
 import { json2query } from ".";
 import { config } from "../config";
 
@@ -8,38 +6,6 @@ let basePath =
   config.env == "local"
     ? `http://${config.LOCAL_API_URL}`
     : `https://${config.API_URL}`;
-
-const OBJKT_API_URL = `https://data.objkt.com/v2/graphql`;
-
-const graphQLClient = new GraphQLClient(OBJKT_API_URL, {
-  headers: {}
-});
-
-    const query = gql`
-         query {
-            listing(where: {fa_contract: {_eq: "KT1CwSgYmZewFazZsW348RAQYn1nthiGP3Qa"}, status: {_eq: "active"}}, order_by: {price: asc}) {
-              fa_contract
-              amount
-              price
-              token {
-                name
-                token_id
-                display_uri
-              }
-              status
-            }
-          `;
-   
-
-export const getPrice = async() => {
-  const { data, isLoading, error } = useQuery("launches", () => {
-    return request(OBJKT_API_URL, query);
-  });
-  return data;
- }
-
-
-
 
 export const getNFT = async (id) => {
   try {
