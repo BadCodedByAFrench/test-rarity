39f840f9741788e8dc167263733f67f75676e69c
BadCodedByAFrench <100372895+BadCodedByAFrench@users.noreply.github.com>
2022-09-24 13:42:59 +0200

    Create requestsGraphQL.js

---
+++ util/requestsGraphQL.js
@@ -0,0 +1,31 @@
+import { useQuery  } from "react-query";
+import { GraphQLClient, gql } from "graphql-request";
+
+const OBJKT_API_URL = `https://data.objkt.com/v2/graphql`;
+
+const graphQLClient = new GraphQLClient(OBJKT_API_URL, {
+  headers: {}
+});
+
+    const query = gql`
+         query {
+            listing(where: {fa_contract: {_eq: "KT1CwSgYmZewFazZsW348RAQYn1nthiGP3Qa"}, status: {_eq: "active"}}, order_by: {price: asc}) {
+              fa_contract
+              amount
+              price
+              token {
+                name
+                token_id
+                display_uri
+              }
+              status
+            }
+          `;
+   
+
+export const getPrice = async() => {
+  const { data, isLoading, error } = useQuery("launches", () => {
+    return request(OBJKT_API_URL, query);
+  });
+  return data;
+ }
