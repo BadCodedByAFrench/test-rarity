3e4bfad3d825f27b399516fa1a827eed506b2326
BadCodedByAFrench <100372895+BadCodedByAFrench@users.noreply.github.com>
2022-09-24 11:46:54 +0200

    Update requests.js

--- util/requests.js
+++ util/requests.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useQuery } from "react-query";
+import { GraphQLClient, gql } from "graphql-request";
 import { json2query } from ".";
 import { config } from "../config";
 
@@ -6,6 +8,35 @@ let basePath =
   config.env == "local"
     ? `http://${config.LOCAL_API_URL}`
     : `https://${config.API_URL}`;
+
+const API_URL = `https://data.objkt.com/v2/graphql`;
+
+const graphQLClient = new GraphQLClient(API_URL, {
+  headers: {}
+});
+
+export function useListedPrice() {
+  return useQuery("get-ListedPrice", async () => {
+    const { getListedPrice } = await graphQLClient.request(gql`
+      query {
+            listing(where: {fa_contract: {_eq: "KT1CwSgYmZewFazZsW348RAQYn1nthiGP3Qa"}, status: {_eq: "active"}}, order_by: {price: asc}) {
+              fa_contract
+              amount
+              price
+              token {
+                name
+                token_id
+                display_uri
+              }
+              status
+            }
+          }
+    `);
+    return getListedPrice;
+  });
+}
+
+
 
 export const getNFT = async (id) => {
   try {
@@ -36,3 +67,12 @@ export const getNFTInfo = async (id) => {
   const data = await res.json();
   return data;
 };
+
+export const getNFTPrice = async (id) => {
+  const res = await fetch(
+    `https://ipfs.io/ipfs/QmSawZEASXSak5HrhxrKH5YH2fBmshZE3gkU6zxBcBuSm7/${id}.json`
+  );
+  const data = await res.json();
+  return data;
+};
+
