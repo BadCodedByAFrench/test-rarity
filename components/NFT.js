a0dc44acb81b9c24a964b1c3594706a1b1c9ea14
BadCodedByAFrench <100372895+BadCodedByAFrench@users.noreply.github.com>
2022-09-25 12:53:37 +0200

    Update NFT.js

--- components/NFT.js
+++ components/NFT.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export const NFT = (nft) => {
   console.log(nft);
+  const value : nft != "Not to sale" ? " XTZ", "";
   return (
     <>
       <a
@@ -27,7 +28,7 @@ export const NFT = (nft) => {
           <h3 className="text-xs text-gray-600">#{nft.id}</h3>
         </div>
          <div className="rounded-b-md py-2 px-2">
-          <h3 className="text-xs text-gray-600">Price: {nft.price}</h3>
+          <h3 className="text-xs text-gray-600">Price: {nft.price}{value}</h3>
         </div>
       </a>
     </>
