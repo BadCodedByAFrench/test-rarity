309de9f2df903682849a9ca6c553f4f55e92d74a
BadCodedByAFrench <100372895+BadCodedByAFrench@users.noreply.github.com>
2022-09-27 17:51:44 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import 'tailwindcss/tailwind.css'
+import { getPriceV2 } from "../util/requestsGraphQL.js";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export const allPrices = setInterval(()=> getPriceV2(0, true), 60*1*1000);
+
 
 export default MyApp
