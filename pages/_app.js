2a68a9cc32eb497a954c6183ba53b4a9b4466cb2
BadCodedByAFrench <100372895+BadCodedByAFrench@users.noreply.github.com>
2022-09-27 18:43:55 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export const allPrices = setInterval(()=>  getPriceV2(0, true), 60*1*1000);
+export const allPrices = setInterval(()=>  getPriceV2(0, true), 60*5*1000);
 
 
 export default MyApp
