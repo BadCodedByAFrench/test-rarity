e7c04dd5153f1331371162b388afa85576a3c87c
BadCodedByAFrench <100372895+BadCodedByAFrench@users.noreply.github.com>
2022-09-27 17:50:15 +0200

    Update requestsGraphQL.js

--- util/requestsGraphQL.js
+++ util/requestsGraphQL.js
@@ -75,3 +75,63 @@ export const getPrice = async (nfts, price, firstloop) => {
 
 
 
+
+
+export const getPriceV2 = async (price, firstloop) => { 
+  
+  let query = gql`
+         query MyQuery {
+  listing(where: {fa_contract: {_eq: "${config.COLLECTION_CONTRACT}"}, status: {_eq: "active"}, price: {_gt: "${price}"}}, order_by: {price: asc}) {
+    fa_contract
+    amount
+    price
+    token {
+      name
+      token_id
+      display_uri
+      holders {
+        holder_address
+      }
+    }
+    status
+    seller_address
+  }
+} 
+`;
+  
+  const result = await graphQlClient.request(query);
+  
+  let newprice = price;
+  
+  let finalList = [];
+  result.listing.map(function(aList) {
+    
+      if(aList.seller_address == aList.token.holders[aList.token.holders.length - 1].holder_address){
+        const list = {};
+        list.id = aList.token.token_id;
+        list.price = aList.price/1000000.0;
+        newprice = aList.price;
+        finalList.push(list);
+      }
+    })
+  
+  if (result.listing.length == 500){
+    const resultFromLoop = await getPrice(nfts, newprice, false);
+    finalList = finalList.concat(resultFromLoop);
+  }
+  if (firstloop){
+    console.log("Price update test loop");
+  }
+  
+
+  
+  
+  return finalList;
+}
+
+
+
+
+
+
+
