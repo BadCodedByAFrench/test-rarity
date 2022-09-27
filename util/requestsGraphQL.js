3759a2aa8b35ed59217c52b3a40037ab17e265d8
BadCodedByAFrench <100372895+BadCodedByAFrench@users.noreply.github.com>
2022-09-27 18:40:55 +0200

    Update requestsGraphQL.js

--- util/requestsGraphQL.js
+++ util/requestsGraphQL.js
@@ -7,72 +7,7 @@ const graphQLClient = new GraphQLClient(OBJKT_API_URL, {
   headers: {}
 });
    
-const graphQlClient = new GraphQLClient(OBJKT_API_URL, { headers: {} })
-
-export const getPrice = async (nfts, price, firstloop) => { 
-  
-  let query = gql`
-         query MyQuery {
-  listing(where: {fa_contract: {_eq: "${config.COLLECTION_CONTRACT}"}, status: {_eq: "active"}, price: {_gt: "${price}"}}, order_by: {price: asc}) {
-    fa_contract
-    amount
-    price
-    token {
-      name
-      token_id
-      display_uri
-      holders {
-        holder_address
-      }
-    }
-    status
-    seller_address
-  }
-} 
-`;
-  
-  const result = await graphQlClient.request(query);
-  
-  let newprice = price;
-  
-  let finalList = [];
-  result.listing.map(function(aList) {
-    
-      if(aList.seller_address == aList.token.holders[aList.token.holders.length - 1].holder_address){
-        const list = {};
-        list.id = aList.token.token_id;
-        list.price = aList.price/1000000.0;
-        newprice = aList.price;
-        finalList.push(list);
-      }
-    })
-  
-  if (result.listing.length == 500){
-    const resultFromLoop = await getPrice(nfts, newprice, false);
-    finalList = finalList.concat(resultFromLoop);
-  }
-  if (firstloop){
-      nfts.map(function(nft) {
-      nft.price = "Not to sale";
-     
-      finalList.map(function(aPrice) {
-        if(parseInt(aPrice.id,10) == nft.id){
-          nft.price = aPrice.price;
-        }
-        })
-    })
-    console.log("Price update");
-  }
-  
-
-  
-  
-  return finalList;
-}
-
-
-
-
+const graphQlClient = new GraphQLClient(OBJKT_API_URL, { headers: {} });
 
 export let allThePrices = [];
 export let firstQuery = true;
